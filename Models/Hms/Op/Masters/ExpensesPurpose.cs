2e9e5bfbd01d8cea64b76e32026741671a07ff6d
Ramu <bandari.ramu5@gmail.com>
2025-11-19 15:42:18 +0530

    Updated forms

---
+++ Models/Hms/Op/Masters/ExpensesPurpose.cs
@@ -0,0 +1,22 @@
+﻿namespace HanuMediSoftCore.Models.Hms.Op.Masters
+{
+    public class ExpensesPurpose
+    {
+        public string? ExpensesPurposeId { get; set; }
+        public string? ExpensesPurposeName { get; set; }
+        public string? ExpensesPurposeCode { get; set; }
+        public int IsActive { get; set; }
+
+        public string? CreatedAt { get; set; }
+        public string? UpdatedAt { get; set; }
+        public string? CreatedById { get; set; }
+        public string? CreatedByName { get; set; }
+        public string? UpdatedById { get; set; }
+        public string? UpdatedByName { get; set; }
+        public string? WorkstationId { get; set; }
+
+        // Paging
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 50;
+    }
+}
---
+++ Models/Hms/Op/Masters/ExpensesType.cs
@@ -0,0 +1,22 @@
+﻿namespace HanuMediSoftCore.Models.Hms.Op.Masters
+{
+    public class ExpensesType
+    {
+        public string? ExpensesTypeId { get; set; }
+        public string? ExpensesTypeName { get; set; }
+        public string? ExpensesTypeCode { get; set; }
+        public int IsActive { get; set; }
+
+        public string? CreatedAt { get; set; }
+        public string? UpdatedAt { get; set; }
+        public string? CreatedById { get; set; }
+        public string? CreatedByName { get; set; }
+        public string? UpdatedById { get; set; }
+        public string? UpdatedByName { get; set; }
+        public string? WorkstationId { get; set; }
+
+        // Paging
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 50;
+    }
+}
