1ba6e0a28d27c569b9e7a883f61b7b051cfa40f3
Ramu <bandari.ramu5@gmail.com>
2025-11-25 16:00:56 +0530

    Updated

---
+++ Models/Hms/Op/Masters/InsuranceMaster.cs
@@ -0,0 +1,12 @@
+﻿namespace HanuMediSoftCore.Models.Hms.Op.Masters
+{
+    public class InsuranceMaster
+    {
+
+                public string? InsuranceId  { get; set; }
+                public string? InsuranceName  { get; set; }
+                public int? PageIndex { get; set; }
+                public int?  PageSize { get; set; }
+
+    }
+}
---
+++ Models/Hms/Op/Masters/SubInsurance.cs
@@ -0,0 +1,21 @@
+﻿namespace HanuMediSoftCore.Models.Hms.Op.Masters
+{
+    public class SubInsurance
+    {
+        public string? SubInsuranceId { get; set; }
+        public string? SubInsuranceName { get; set; }
+        public string? SubInsuranceCode { get; set; }
+        public string? InsuranceId { get; set; }
+
+        public int IsActive { get; set; }
+
+        public string? CreatedById { get; set; }
+        public string? CreatedByName { get; set; }
+        public string? UpdatedById { get; set; }
+        public string? UpdatedByName { get; set; }
+        public string? WorkstationId { get; set; }
+
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+    }
+}
